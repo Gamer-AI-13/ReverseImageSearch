6a6fbb68bf9702a9a8e9412f95f61290ae0fc263
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-11-18 21:10:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    CLIENT_ID = ""
-    BOT_TOKEN = ""
-    HEROKU_APP_NAME = ""
+    CLIENT_ID = environ.get('CLIENT_ID')
+    BOT_TOKEN = environ.get('BOT_TOKEN')
+    HEROKU_APP_NAME = environ.get('HEROKU_APP_NAME')
